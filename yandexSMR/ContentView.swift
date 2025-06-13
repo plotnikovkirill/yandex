e6329960c6bddb6cadcb9467b703340479fc4d44
plotnikovkirill <plotnikov.kirillka2005@yadnex.ru>
2025-06-13 09:43:39 -0700

    Initial Commit

---
+++ yandexSMR/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  yandexSMR
+//
+//  Created by kirill on 13.06.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ yandexSMRTests/yandexSMRTests.swift
@@ -0,0 +1,17 @@
+//
+//  yandexSMRTests.swift
+//  yandexSMRTests
+//
+//  Created by kirill on 13.06.2025.
+//
+
+import Testing
+@testable import yandexSMR
+
+struct yandexSMRTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
