04a18394bbf6dbf081fff4302f2a87c90aa2519a
plotnikovkirill <plotnikov.kirillka2005@yadnex.ru>
2025-06-13 09:43:39 -0700

    Initial Commit

---
+++ yandexSMR/yandexSMRApp.swift
@@ -0,0 +1,17 @@
+//
+//  yandexSMRApp.swift
+//  yandexSMR
+//
+//  Created by kirill on 13.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct yandexSMRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
